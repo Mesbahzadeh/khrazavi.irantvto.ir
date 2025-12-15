aa30fc89dd4e1146f03e6f2401df0da1300699b2
Mehran Mesbahzadeh <mehran.mesbahzadeh@yahoo.com>
2025-12-15 15:08:21 +0330

    end

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is crucial for GitHub Pages to load assets correctly
+  base: '/khrazavi.irantvto.ir/',
 });
